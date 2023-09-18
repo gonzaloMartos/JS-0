37c7f7cf9d2c98e609d5944700cfb7f460ab45a1
gonzaloMartos <gonzalomartos2002@gmail.com>
2023-09-18 18:51:26 -0300

    JS-0

---
+++ app.js
@@ -0,0 +1,35 @@
+//Strings
+let string1 = "Me llamo Gonzalo";
+let string2 = "Me gusta programar";
+let string3 = "Tengo un perro";
+let string4 = "Pelota";
+let string5 = "No juego basquetball";
+let string6 = "Pero me gusta";
+let string7 = "Palabras";
+let string8 = "Videojuegos";
+let string9 = "Uso mucho google";
+let string10 = "Chat gpt es buenisimo";
+
+//Numbers
+let number1 = 10;
+let number2 = 20;
+let number3 = 30;
+let number4 = 40;
+let number5 = 50;
+let number6 = 60;
+let number7 = 70;
+let number8 = 80;
+let number9 = 90;
+let number10 = 100;
+
+//Booleanos
+let boolean1 = true;
+let boolean2 = false;
+let boolean3 = true;
+let boolean4 = false;
+let boolean5 = true;
+let boolean6 = false;
+let boolean7 = true;
+let boolean8 = false;
+let boolean9 = true;
+let boolean10 = false;
